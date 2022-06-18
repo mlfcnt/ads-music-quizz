c15179dccc6dd19f0226165f1734edebebe327a6
TMN <t.martin@adsoftware.fr>
2022-06-18 18:36:33 +0200

    test

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import { LowSync, JSONFileSync } from "lowdb";
 
-const db = new LowSync(new JSONFileSync("db.json"));
+const db = new LowSync(new JSONFileSync("tmp/db.json"));
 db.read();
 
 export default db;
