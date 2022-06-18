6e7237423878788921f92477829c63768d3ee460
TMN <t.martin@adsoftware.fr>
2022-06-18 18:55:18 +0200

    test

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import { LowSync, JSONFileSync } from "lowdb";
 import path from "path";
 
-const file = path.join(process.cwd(), "db.json");
+const file = path.join(process.cwd(), "public", "db.json");
 const db = new LowSync(new JSONFileSync(file));
 
 db.read();
