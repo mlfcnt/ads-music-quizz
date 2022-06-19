2c32f3698e6d69851500ec58b9fffc4c35bf4a30
TMN <t.martin@adsoftware.fr>
2022-06-19 23:00:32 +0200

    bandage

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Track } from "../api/spotify";
-import { useOembedPlayer } from "../hooks/useOembedPlayer";
 
 export const Player = ({ trackId }: { trackId: Track["id"] }) => {
-  const oEmbed = useOembedPlayer(trackId);
-
-  const createMarkup = (html: any) => ({ __html: html });
-
-  return oEmbed?.html ? (
-    <div
-      dangerouslySetInnerHTML={createMarkup(oEmbed?.html)}
-      className="spotify-oembed"
-    />
-  ) : null;
+  return (
+    <iframe
+      style={{ borderRadius: "12px", left: "50%" }}
+      src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0?autoplay=true`}
+      width="80px"
+      height="80"
+      frameBorder="0"
+      allowFullScreen={true}
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    ></iframe>
+  );
 };
