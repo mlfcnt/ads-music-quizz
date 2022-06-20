a83e9a6d9ee7d58639d1d31a55aa9695d4e02167
TMN <t.martin@adsoftware.fr>
2022-06-20 21:00:27 +0200

    moche mais au moins... jouable

--- components/Player.tsx
+++ components/Player.tsx
@@ -6,11 +6,7 @@ export const Player = ({ trackId }: { trackId: Track["id"] }) => {
   return (
     <Box>
       <iframe
-        style={{
-          borderRadius: "100px",
-          opacity: "0.8",
-          border: "2px dotted transparent",
-        }}
+        className="frame-spotify"
         src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0`}
         width="80px"
         height="80"
