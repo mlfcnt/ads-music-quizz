63acf371025d00eb5d5a7a9d1d5b8358f2430cdf
TMN <t.martin@adsoftware.fr>
2022-06-21 08:15:03 +0200

    mobile

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,11 +1,14 @@
 import { Box } from "@mantine/core";
 import React from "react";
 import { Track } from "../api/spotify";
+import { useMediaQuery } from "@mantine/hooks";
 
 export const Player = ({ trackId }: { trackId: Track["id"] }) => {
+  const isDesktop = useMediaQuery("(min-width: 900px)", false);
   return (
     <Box>
       <iframe
+        style={{ marginLeft: isDesktop ? "25rem" : "8.1rem" }}
         className="frame-spotify"
         src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0`}
         width="80px"
